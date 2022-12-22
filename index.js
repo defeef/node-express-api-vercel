eca7e8228af05e808d5d55ee7873d63012f7d89a
defeef <henrybuild@roadrunner.com>
2022-12-21 22:00:52 -0500

    Test about

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render("/views/pages/about.ejs");
+  res.send("About");
+  //res.render("/views/pages/about.ejs");
 })
 
 // Export the Express API
