8075c5286a80dfe7e1c8fd2ecc60eae0bc93992d
defeef <henrybuild@roadrunner.com>
2022-12-22 19:40:24 -0500

    change import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express()
 const PORT = 4000
@@ -23,4 +23,4 @@ app.listen(PORT, () => {
 })
 
 // Export the Express API
-module.exports = app
+export default app
